251870c11c9d36aff7148cb64c603a186c4e24b8
Malik <97180802+mjones97@users.noreply.github.com>
2024-09-04 15:41:04 -0400

    Update ToDoItem.jsx

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 
 const ToDoItem = ({ todo, toggleTodo, removeTodo }) => {
-  return (
-    <li>
-        <span 
-            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }} 
-            onClick={() => toggleTodo(todo.id)}
-        >
-            {todo.text}
-        </span>
-        <button onClick={() => removeTodo(todo.id)}>Delete</button>
-    </li>
-  )
+    return (
+        <div>
+            <li>
+                <span 
+                    style={{ textDecoration: todo.completed ? 'line-through' : 'none' }} 
+                    onClick={() => toggleTodo(todo.id)}
+                >
+                    {todo.text}
+                </span>
+                <button className='delete-btn' onClick={() => removeTodo(todo.id)}>Delete</button>
+            </li>
+            <div className="divider"></div>
+        </div>
+    )
 }
 
 export default ToDoItem
